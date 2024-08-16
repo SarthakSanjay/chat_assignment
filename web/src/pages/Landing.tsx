056c0077deb8c907f98a8992c9106ed070752498
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-08-16 23:44:48 +0530

    init landing page

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='w-screen h-screen bg-[#ef6145] text-black flex justify-center items-center'>Landing</div>
+  )
+}
+
+export default Landing
